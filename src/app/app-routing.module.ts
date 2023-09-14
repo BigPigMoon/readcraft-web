356854b5a7cc2e73e08e1c41485ddc01dd7529fe
bigpigmoon <matvey.novikov02@gmail.com>
2023-09-14 12:37:21 +0700

    create all main pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CardsPageComponent} from './pages/cards-page/cards-page.component';
+import {WordsPageComponent} from './pages/words-page/words-page.component';
+import {LibraryPageComponent} from './pages/library-page/library-page.component';
+import {StatisticsPageComponent} from './pages/statistics-page/statistics-page.component';
+import {BookPageComponent} from "./pages/book-page/book-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CardsPageComponent},
+  {path: 'words', component: WordsPageComponent},
+  {path: 'library', component: LibraryPageComponent},
+  {path: 'statistic', component: StatisticsPageComponent},
+  {path: 'book/:id', component: BookPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {StatisticsPageComponent} from './pages/statistics-page/statistics-page.component';
+import {SignupPageComponent} from './pages/signup-page/signup-page.component';
+import {SigninPageComponent} from './pages/signin-page/signin-page.component';
+import {NavigationComponent} from './components/navigation/navigation.component';
+import {LearningWordTableComponent} from './components/learning-word-table/learning-word-table.component';
+import {LearningStatisticComponent} from './components/learning-statistic/learning-statistic.component';
+import {WordsPageComponent} from './pages/words-page/words-page.component';
+import {LibraryPageComponent} from './pages/library-page/library-page.component';
+import {CardsPageComponent} from './pages/cards-page/cards-page.component';
+import {BookCardComponent} from './components/book-card/book-card.component';
+import {NgOptimizedImage} from "@angular/common";
+import { SerachbarComponent } from './components/serachbar/serachbar.component';
+import { WordCardComponent } from './components/word-card/word-card.component';
+import { BookPageComponent } from './pages/book-page/book-page.component';
+import { ReaderPageComponent } from './pages/reader-page/reader-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatisticsPageComponent,
+    SignupPageComponent,
+    SigninPageComponent,
+    NavigationComponent,
+    LearningWordTableComponent,
+    LearningStatisticComponent,
+    WordsPageComponent,
+    LibraryPageComponent,
+    CardsPageComponent,
+    BookCardComponent,
+    SerachbarComponent,
+    WordCardComponent,
+    BookPageComponent,
+    ReaderPageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgOptimizedImage],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
