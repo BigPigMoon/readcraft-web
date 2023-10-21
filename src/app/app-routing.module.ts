b71120c6b66402f074bea24eca093f465da61d21
bigpigmoon <matvey.novikov02@gmail.com>
2023-10-21 14:09:43 +0700

    refactor: remove word page and add graph page!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,21 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {CardsPageComponent} from './pages/cards-page/cards-page.component';
-import {WordsPageComponent} from './pages/words-page/words-page.component';
-import {LibraryPageComponent} from './pages/library-page/library-page.component';
-import {StatisticsPageComponent} from './pages/statistics-page/statistics-page.component';
-import {BookPageComponent} from "./pages/book-page/book-page.component";
-import {ReaderPageComponent} from "./pages/reader-page/reader-page.component";
-import {TestPageComponent} from "./pages/test-page/test-page.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CardsPageComponent } from './pages/cards-page/cards-page.component';
+import { LibraryPageComponent } from './pages/library-page/library-page.component';
+import { BookPageComponent } from './pages/book-page/book-page.component';
+import { ReaderPageComponent } from './pages/reader-page/reader-page.component';
+import { GraphPageComponent } from './pages/graph-page/graph-page.component';
 
 const routes: Routes = [
-  {path: '', component: CardsPageComponent},
-  {path: 'words', component: WordsPageComponent},
-  {path: 'library', component: LibraryPageComponent},
-  {path: 'statistic', component: StatisticsPageComponent},
-  {path: 'book/:id', component: BookPageComponent},
-  {path: 'book/:id/reader', component: ReaderPageComponent,},
-  {path: 'test', component: TestPageComponent}
+  { path: '', component: CardsPageComponent },
+  { path: 'words', component: GraphPageComponent },
+  { path: 'library', component: LibraryPageComponent },
+  { path: 'book/:id', component: BookPageComponent },
+  { path: 'book/:id/reader', component: ReaderPageComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,37 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {StatisticsPageComponent} from './pages/statistics-page/statistics-page.component';
-import {SignupPageComponent} from './pages/signup-page/signup-page.component';
-import {SigninPageComponent} from './pages/signin-page/signin-page.component';
-import {NavigationComponent} from './components/navigation/navigation.component';
-import {LearningWordTableComponent} from './components/learning-word-table/learning-word-table.component';
-import {LearningStatisticComponent} from './components/learning-statistic/learning-statistic.component';
-import {WordsPageComponent} from './pages/words-page/words-page.component';
-import {LibraryPageComponent} from './pages/library-page/library-page.component';
-import {CardsPageComponent} from './pages/cards-page/cards-page.component';
-import {BookCardComponent} from './components/book-card/book-card.component';
-import {NgOptimizedImage} from "@angular/common";
-import {SerachbarComponent} from './components/serachbar/serachbar.component';
-import {WordCardComponent} from './components/word-card/word-card.component';
-import {BookPageComponent} from './pages/book-page/book-page.component';
-import {ReaderPageComponent} from './pages/reader-page/reader-page.component';
-import {NavigationWrapperComponent} from './components/navigation-wrapper/navigation-wrapper.component';
-import {FormsModule} from "@angular/forms";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { SigninPageComponent } from './pages/signin-page/signin-page.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { LearningWordTableComponent } from './components/learning-word-table/learning-word-table.component';
+import { LearningStatisticComponent } from './components/learning-statistic/learning-statistic.component';
+import { LibraryPageComponent } from './pages/library-page/library-page.component';
+import { CardsPageComponent } from './pages/cards-page/cards-page.component';
+import { BookCardComponent } from './components/book-card/book-card.component';
+import { NgOptimizedImage } from '@angular/common';
+import { SerachbarComponent } from './components/serachbar/serachbar.component';
+import { WordCardComponent } from './components/word-card/word-card.component';
+import { BookPageComponent } from './pages/book-page/book-page.component';
+import { ReaderPageComponent } from './pages/reader-page/reader-page.component';
+import { NavigationWrapperComponent } from './components/navigation-wrapper/navigation-wrapper.component';
+import { FormsModule } from '@angular/forms';
 import { ReaderTextComponent } from './components/reader-text/reader-text.component';
 import { ReaderWordComponent } from './components/reader-word/reader-word.component';
 import { FeatureFilterPipe } from './pipes/feature-filter.pipe';
-import { TestPageComponent } from './pages/test-page/test-page.component';
+import { GraphPageComponent } from './pages/graph-page/graph-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StatisticsPageComponent,
     SignupPageComponent,
     SigninPageComponent,
     NavigationComponent,
     LearningWordTableComponent,
     LearningStatisticComponent,
-    WordsPageComponent,
     LibraryPageComponent,
     CardsPageComponent,
     BookCardComponent,
@@ -47,11 +43,16 @@ import { TestPageComponent } from './pages/test-page/test-page.component';
     ReaderTextComponent,
     ReaderWordComponent,
     FeatureFilterPipe,
-    TestPageComponent,
+    GraphPageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgOptimizedImage, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgOptimizedImage,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
